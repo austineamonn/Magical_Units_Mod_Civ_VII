7772accad70d8a7cc572056e10d17b2f0ce3edd2
austineamonn <nicolasa@grinnell.edu>
2025-09-21 01:02:59 -0700

    contains four new magical units. abilities and text completed. not yet added to tech tree.

---
+++ magical-units/ui/game.js
@@ -0,0 +1,38 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_CULTIST': { //your unit type
+        unitType: 'UNIT_HOPLITE',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_CULTIST_2': { //your unit type
+        unitType: 'UNIT_HOPLITE',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_DRAGON': { //your unit type
+        unitType: 'UNIT_PURABHETTARAH',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_LIVING_FOREST': { //your unit type
+        unitType: 'UNIT_MEDJAY',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_LIVING_FOREST_2': { //your unit type
+        unitType: 'UNIT_MEDJAY',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_LIVING_FOREST_3': { //your unit type
+        unitType: 'UNIT_MEDJAY',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_MAGE': { //your unit type
+        unitType: 'UNIT_HULCHE',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+ 
+}
+
+
+
+
+
+
